cdbd35c0deab642cba6430472f3719aeaa8a0b31
Sergey <spushkovskiy@gmail.com>
2024-02-25 18:56:56 +0200

    Installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import "./scss/reset.scss";
+import Home from "./pages/Home";
+import Men from "./pages/Men";
 
 function App() {
-    return(
-        <h1>Hello world</h1>
-  )
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="men" element={<Men />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
