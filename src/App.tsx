9e52baf47695d7c533f5d072014ee8f5d25ee4b0
Sergey <spushkovskiy@gmail.com>
2024-02-26 19:59:54 +0200

    Added 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import "./scss/globals.scss";
 import Home from "./pages/Home";
 import Men from "./pages/Men";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="men" element={<Men />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>This is the 404 page</div>;
+}
